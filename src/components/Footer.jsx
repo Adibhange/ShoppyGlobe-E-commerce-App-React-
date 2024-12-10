a922d8f76ec33c7eb131398cc1be26522639785e
Adibhange <bhangeaditya13@gmail.com>
2024-12-10 15:42:52 +0530

    Create Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="mt-6 bg-foreground px-4 py-4 text-center text-copy-light">
+      <h1>&copy;2024 Aditya Bhange | All rights reserved.</h1>
+    </footer>
+  );
 };
 
 export default Footer;
